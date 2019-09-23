4ee8b9eb5d91effccdaee553a017e45f5c9e049d
danishnxt <danishnxt@gmail.com>
2019-09-23 13:40:24 -0400

    cmmted

--- src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
@@ -292,23 +292,23 @@ public class PairsPMI extends Configured implements Tool {
 
     /////////////// JOB B META DATA ///////////////
 
-    Job job2 = Job.getInstance(conf);
-    job2.setJobName(PairsPMI.class.getSimpleName());
-    job2.setJarByClass(PairsPMI.class);
-    job2.setNumReduceTasks(args.numReducers);
+    // Job job2 = Job.getInstance(conf);
+    // job2.setJobName(PairsPMI.class.getSimpleName());
+    // job2.setJarByClass(PairsPMI.class);
+    // job2.setNumReduceTasks(args.numReducers);
 
-    FileInputFormat.setInputPaths(job2, new Path(args.input));
-    FileOutputFormat.setOutputPath(job2, new Path(args.output));
+    // FileInputFormat.setInputPaths(job2, new Path(args.input));
+    // FileOutputFormat.setOutputPath(job2, new Path(args.output));
 
-    job2.setMapOutputKeyClass(Text.class);
-    job2.setMapOutputValueClass(IntWritable.class);
-    job2.setOutputKeyClass(Text.class);
-    job2.setOutputValueClass(IntWritable.class);
-    job2.setOutputFormatClass(TextOutputFormat.class);
+    // job2.setMapOutputKeyClass(Text.class);
+    // job2.setMapOutputValueClass(IntWritable.class);
+    // job2.setOutputKeyClass(Text.class);
+    // job2.setOutputValueClass(IntWritable.class);
+    // job2.setOutputFormatClass(TextOutputFormat.class);
     
-    job.setMapperClass(MyMapperB.class);
-    // job.setCombinerClass(MyReducerB.class);
-    job.setReducerClass(MyReducerB.class);
+    // job.setMapperClass(MyMapperB.class);
+    // // job.setCombinerClass(MyReducerB.class);
+    // job.setReducerClass(MyReducerB.class);
 
     // Delete the output directory if it exists already.
     Path outputDir = new Path(args.output);
