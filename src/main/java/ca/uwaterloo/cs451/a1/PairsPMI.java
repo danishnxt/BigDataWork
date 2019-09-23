57789d667e273dd87d8b122feeef2b2422d1fcb9
danishnxt <danishnxt@gmail.com>
2019-09-23 14:48:56 -0400

    dwwdwdw

--- src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
@@ -226,7 +226,7 @@ public class PairsPMI extends Configured implements Tool {
        SUM.set(sum);
 
       dbl_result.set(to_log);
-      context.write(key, sum);
+      context.write(key, SUM);
       
     }
   }
