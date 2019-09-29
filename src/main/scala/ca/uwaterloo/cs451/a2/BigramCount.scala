226b95b12c9378993fcba653f957f4fdf7e74a80
danishnxt <danishnxt@gmail.com>
2019-09-29 13:33:08 -0400

    Alpha

--- src/main/scala/ca/uwaterloo/cs451/a2/BigramCount.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/BigramCount.scala
@@ -70,9 +70,10 @@ object BigramCount extends Tokenizer {
     }).map(bigram => (bigram, 1))
     .reduceByKey(_+_)
 
-    val myDict = Map()
+    val myDict = Map[String, Int]()
 
-    beta4.map((a,b) => myDict(a) = b) // update the map please let this work :)
+    beta4.map((a, b) => myDict(a) = b) // update the map please let this work :)
+    beta4.map((a:String, b:Int) => myDict(a) = b) // update the map please let this work :)
     println(myDict) // hello there
     // beta4.saveAsTextFile(args.output())
     
