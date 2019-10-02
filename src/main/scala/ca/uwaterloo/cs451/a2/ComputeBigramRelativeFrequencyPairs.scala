75bb05fca5234d600b86e619b60a219dc5e0bfd8
danishnxt <danishnxt@gmail.com>
2019-10-02 13:44:28 -0400

    alpha

--- src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs.scala
+++ src/main/scala/ca/uwaterloo/cs451/a2/ComputeBigramRelativeFrequencyPairs.scala
@@ -92,7 +92,7 @@ object ComputeBigramRelativeFrequencyPairs extends Tokenizer {
       }
     })
 
-    reducedFinal.saveAsTextFile(args.output())
+    reducedFinal.collect.saveAsTextFile(args.output())
 
   }
 }
