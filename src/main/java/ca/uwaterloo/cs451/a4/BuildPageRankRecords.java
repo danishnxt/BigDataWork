797ebdbd43851bc60757a4d0af193cbd79440aca
danishnxt <danishnxt@gmail.com>
2019-10-26 18:20:14 -0400

    Final Push Assignment 3

--- src/main/java/ca/uwaterloo/cs451/a4/BuildPageRankRecords.java
+++ src/main/java/ca/uwaterloo/cs451/a4/BuildPageRankRecords.java
@@ -90,7 +90,7 @@ public class BuildPageRankRecords extends Configured implements Tool {
 
       System.out.println("hello 4");
 
-      for (int i = 0; i < source_strings.length; i++) {
+      for (int i = 0; i < 3; i++) {
         sources.add(Integer.parseInt(source_strings[i])); // get the list and have it parsed here
       }
 
