adca51f34ac5d453767860f961a4bd345b0129cc
danishnxt <danishnxt@gmail.com>
2019-11-04 20:04:38 -0400

    FINAL PUSH - AS4 COMPLETE

--- src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q1.scala
@@ -40,7 +40,7 @@ object Q1 {
     if (textBool == true) {
       val textFile = sc.textFile(folder + "/lineitem.tbl") // import from the file directly
       val allEntriesA = textFile.filter(entry => (entry.split('|')(10)).substring(0,dateLength) == date)
-      finalVal = (allEntriesA.map(line => (1, 1)).reduce(_+_))(1)
+      val finalVal = (allEntriesA.map(line => (1, 1)).reduce(_+_))(1)
       println("CHECK VALUE -> ")
       println("CHECK VALUE -> ")
       println("CHECK VALUE -> ")
