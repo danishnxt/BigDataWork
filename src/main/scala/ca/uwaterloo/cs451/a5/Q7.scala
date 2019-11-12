e8b78ffdeacc2ad673bfebbbe1f20c2f346b04a1
danishnxt <danishnxt@gmail.com>
2019-11-12 15:06:07 -0400

    FINAL PUSH - AS4 COMPLETE

--- src/main/scala/ca/uwaterloo/cs451/a5/Q7.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/Q7.scala
@@ -71,9 +71,9 @@ object Q7 {
             var itrB = beta._2.iterator
 
             while (itrA.hasNext) {
-              val ord = itrA.next // get value out
+              val ord = itrA.next() // get value out
               while (itrB.hasNext) {
-                dList += (alpha -> (ord._1, ord._2, ord._3, itrB.next))
+                dList += (alpha -> (ord._1, ord._2, ord._3, itrB.next()))
               }
             }
             dLlist
@@ -92,42 +92,42 @@ object Q7 {
 
       // get moniker is a shorter code to get the same thing done as prev questions -> useful, will not be changing previous questions however, they are functionally correct
 
-      val sparkSession = SparkSession.builder.getOrCreate
-
-      val rddFileItem = (sparkSession.read.parquet(folder + "/lineitem")).rdd // read for a parquet file
-      val rddFileCustomer = (sparkSession.read.parquet(folder + "/customer")).rdd // read for a parquet file
-      val rddFileOrders = (sparkSession.read.parquet(folder + "/orders")).rdd // read for a parquet file
-
-      var lineItem_Rec = rddFileItem.map(entry => (entry.getInt(0), entry.getDouble(5), entry.getDouble(6), entry.getString(10))).filter(entry -> entry._4 > date).map(entry => (entry._1, entry._2*(1 - entry._3)))
-      var order_Rec = rddFileOrders.map(entry => (entry.getInt(0), entry.getInt(1), entry.getString(4), entry.getInt(7))).filter(entry -> entry._3 > date).map(entry => (entry._1, (entry._2, entry._3, entry._4)))
-
-      var customer_Rec = rddFileCustomer.map(entry => (entry.getInt(0), entry.getString(1)))
-      val global_Customer = sc.broadcast(customer_Rec.collectAsMap())
-
-      val FinalValue = orders_Rec.cogroup(lineItem_Rec)
-        .flatmap {
-          case(alpha, beta) =>
-            var dList = new ListBuffer[(Int,(Int, String, Int, Double))]()
-          var itrA = beta._1.iterator
-          var itrB = beta._2.iterator
-
-          while (itrA.hasNext) {
-            val ord = itrA.next // get value out
-            while (itrB.hasNext) {
-              dList += (alpha -> (ord._1, ord._2, ord._3, itrB.next))
-            }
-          }
-          dLlist
-        }
-
-      val retVal = FinalValue.map {
-        //        case (alpha, beta) => ((global_Customer.value.getOrElse(beta._1, null).toString, alpha beta._2, beta._3) -> (beta._4)) #didnt' recog for some reason
-        case (alpha, beta) => ((global_Customer.value.getOrElse(beta._1, null).asInstanceof[String], alpha, beta._2, beta._3) -> (beta._4))
-      }.filter(x => x._1._1 != null).reduceByKey(_+_).map {
-        case (alpha, beta) => (alpha._1, alpha._2, beta, alpha._3, alpha._4)
-      }.collect().sortBy(_._3).reverse().take(10) // descending order
-
-      retVal.foreach(println)
-    }
+//      val sparkSession = SparkSession.builder.getOrCreate
+//
+//      val rddFileItem = (sparkSession.read.parquet(folder + "/lineitem")).rdd // read for a parquet file
+//      val rddFileCustomer = (sparkSession.read.parquet(folder + "/customer")).rdd // read for a parquet file
+//      val rddFileOrders = (sparkSession.read.parquet(folder + "/orders")).rdd // read for a parquet file
+//
+//      var lineItem_Rec = rddFileItem.map(entry => (entry.getInt(0), entry.getDouble(5), entry.getDouble(6), entry.getString(10))).filter(entry -> entry._4 > date).map(entry => (entry._1, entry._2*(1 - entry._3)))
+//      var order_Rec = rddFileOrders.map(entry => (entry.getInt(0), entry.getInt(1), entry.getString(4), entry.getInt(7))).filter(entry -> entry._3 > date).map(entry => (entry._1, (entry._2, entry._3, entry._4)))
+//
+//      var customer_Rec = rddFileCustomer.map(entry => (entry.getInt(0), entry.getString(1)))
+//      val global_Customer = sc.broadcast(customer_Rec.collectAsMap())
+//
+//      val FinalValue = orders_Rec.cogroup(lineItem_Rec)
+//        .flatmap {
+//          case(alpha, beta) =>
+//            var dList = new ListBuffer[(Int,(Int, String, Int, Double))]()
+//          var itrA = beta._1.iterator
+//          var itrB = beta._2.iterator
+//
+//          while (itrA.hasNext) {
+//            val ord = itrA.next // get value out
+//            while (itrB.hasNext) {
+//              dList += (alpha -> (ord._1, ord._2, ord._3, itrB.next))
+//            }
+//          }
+//          dLlist
+//        }
+//
+//      val retVal = FinalValue.map {
+//        //        case (alpha, beta) => ((global_Customer.value.getOrElse(beta._1, null).toString, alpha beta._2, beta._3) -> (beta._4)) #didnt' recog for some reason
+//        case (alpha, beta) => ((global_Customer.value.getOrElse(beta._1, null).asInstanceof[String], alpha, beta._2, beta._3) -> (beta._4))
+//      }.filter(x => x._1._1 != null).reduceByKey(_+_).map {
+//        case (alpha, beta) => (alpha._1, alpha._2, beta, alpha._3, alpha._4)
+//      }.collect().sortBy(_._3).reverse().take(10) // descending order
+//
+//      retVal.foreach(println)
+//    }
   }
 }
